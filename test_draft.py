0c1e1909cec2c0cb2113eb5e3e8c8ed8f9741cbf
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-14 11:56:12 -0700

    init: draft

--- test_draft.py
+++ test_draft.py
@@ -5,7 +5,7 @@ from faker import Faker
 
 TIMEOUT = 0.01
 driver = webdriver.Chrome()
-# first_item = driver.find_element(By.XPATH, '//div[@class="inventory_item"][1]')
+
 
 def test_draft():
     # Precondition, "https://www.saucedemo.com/inventory.html" page
@@ -20,13 +20,22 @@ def test_draft():
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
 
     # Draft for test scenarios
+    onesie_link_name = driver.find_element(By.XPATH, "//div[text()='Sauce Labs Onesie']")
+    onesie_link_name.click()
+    time.sleep(TIMEOUT)
 
-    # Filters
-    name_desc_filter = driver.find_element(By.XPATH, '//option[@value="za"]')
-    name_desc_filter.click()
-    first_item = driver.find_element(By.XPATH, '//div[@class="inventory_item"][1]')
-    first_item_name = first_item.text.splitlines()[2]
-    print(first_item_name)
+    onesie_add_to_cart = driver.find_element(By.XPATH, "//button[@data-test='add-to-cart-sauce-labs-onesie']")
+    onesie_add_to_cart.click()
+    time.sleep(TIMEOUT)
+    shopping_cart_badge = driver.find_element(By.XPATH, "//span[@class='shopping_cart_badge']")
+    assert shopping_cart_badge.text == "1"
+    time.sleep(TIMEOUT)
 
-    assert first_item_name == "Test.allTheThings() T-Shirt (Red)"
+    onesie_remove_from_cart = driver.find_element(By.XPATH, "//button[@data-test='remove-sauce-labs-onesie']")
+    onesie_remove_from_cart.click()
+    time.sleep(TIMEOUT)
+
+    shopping_cart_badge = driver.find_elements(By.XPATH, "//span[@class='shopping_cart_badge']")
+    assert len(shopping_cart_badge) == 0
+
     driver.quit()
