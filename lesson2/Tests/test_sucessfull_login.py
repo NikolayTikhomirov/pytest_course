4d0293048e1654a7a81f4d68ebb688ef160de8ea
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-21 15:17:28 -0700

    refactor: more nice

--- lesson2/Tests/test_sucessfull_login.py
+++ lesson2/Tests/test_sucessfull_login.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from time import sleep
 from locators import USERNAME_FIELD, PASSWORD_FIELD, LOGIN_BUTTON
 from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE, TIMEOUT
@@ -18,4 +17,3 @@ def test_login_form(driver):
 
     sleep(TIMEOUT)
     assert driver.current_url == INVENTORY_PAGE
-
