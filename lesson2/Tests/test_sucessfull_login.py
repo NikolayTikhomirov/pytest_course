d243ffa86a228192b65d99ae3a370d8bdc97a1f8
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-21 15:33:15 -0700

    refactor: delete spaces

--- lesson2/Tests/test_sucessfull_login.py
+++ lesson2/Tests/test_sucessfull_login.py
@@ -1,19 +1,9 @@
-from time import sleep
 from locators import USERNAME_FIELD, PASSWORD_FIELD, LOGIN_BUTTON
-from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE, TIMEOUT
-
+from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE
 
 def test_login_form(driver):
     driver.get(MAIN_PAGE)
-
-    # вводим валидный логин в поле "Username"
     driver.find_element(*USERNAME_FIELD).send_keys(LOGIN)
-
-    # вводим валидный пароль в поле "Password"
     driver.find_element(*PASSWORD_FIELD).send_keys(PASSWORD)
-
-    # кликаем на кнопку "Login"
     driver.find_element(*LOGIN_BUTTON).click()
-
-    sleep(TIMEOUT)
     assert driver.current_url == INVENTORY_PAGE
