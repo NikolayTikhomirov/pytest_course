964e7029a82daf52832f2876b10c0ba5f6a90ecd
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-21 15:34:02 -0700

    feat: add chrome_options

--- lesson2/Tests/conftest.py
+++ lesson2/Tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def chrome_options():
+    options = Options()
+    # options.add_argument('--window-size=100,100')
+    options.add_argument('--incognito')
+    # options.add_argument('--headless')
+    return options
 
 @pytest.fixture()
-def driver():
-    driver = webdriver.Chrome()
+def driver(chrome_options):
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.implicitly_wait(10)
     yield driver
-    print('\nquit browser...')
     driver.quit()
-
