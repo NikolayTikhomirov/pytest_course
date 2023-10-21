9849dec160227cc22f950958fdba67b8eda9d2ab
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-21 15:46:31 -0700

    feat: add logged_in_user fixture

--- lesson2/Tests/conftest.py
+++ lesson2/Tests/conftest.py
@@ -2,6 +2,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from locators import USERNAME_FIELD, PASSWORD_FIELD, LOGIN_BUTTON
+from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE, MAX_TIMEOUT
+
 @pytest.fixture
 def chrome_options():
     options = Options()
@@ -13,6 +16,14 @@ def chrome_options():
 @pytest.fixture()
 def driver(chrome_options):
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(MAX_TIMEOUT)
     yield driver
     driver.quit()
+
+@pytest.fixture()
+def logged_in_user(driver):
+    driver.get(MAIN_PAGE)
+    driver.find_element(*USERNAME_FIELD).send_keys(LOGIN)
+    driver.find_element(*PASSWORD_FIELD).send_keys(PASSWORD)
+    driver.find_element(*LOGIN_BUTTON).click()
+
