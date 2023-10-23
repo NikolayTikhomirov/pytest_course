fed3d9fa9fb0db46a5dbb750b154fa20813ee6e4
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 12:38:57 -0700

    init: global_variables.py

---
+++ lesson3/Tests/test_herokuapp/global_variables.py
@@ -0,0 +1,16 @@
+# Authorization
+LOGIN = "admin"
+PASSWORD = "admin"
+WRONG_LOGIN = "unknown_user"
+WRONG_PASSWORD = "unknown_password"
+
+# urls
+MAIN_PAGE = "https://the-internet.herokuapp.com/"
+ADD_REMOVE_ELEMENTS_PAGE = "https://the-internet.herokuapp.com/add_remove_elements/"
+BASIC_AUTH_PAGE = "https://the-internet.herokuapp.com/basic_auth"
+BROKEN_IMAGES_PAGE = "https://saucelabs.com/"
+CHECKBOXES_PAGE = "https://the-internet.herokuapp.com/checkboxes"
+
+# Timeout
+TIMEOUT = 1
+MAX_TIMEOUT = 10
