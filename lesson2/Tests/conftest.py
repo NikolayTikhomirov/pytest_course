efe8290c03800becb31e51d61c9bac5a095c01e2
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-22 18:58:12 -0700

    test: play with fixtures

--- lesson2/Tests/conftest.py
+++ lesson2/Tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-from locators import USERNAME_FIELD, PASSWORD_FIELD, LOGIN_BUTTON
-from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE, MAX_TIMEOUT
+import locators as l
+import global_variables as gv
+from time import sleep
 
 @pytest.fixture
 def chrome_options():
@@ -13,16 +13,33 @@ def chrome_options():
     # options.add_argument('--headless')
     return options
 
-@pytest.fixture()
+@pytest.fixture
 def driver(chrome_options):
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(MAX_TIMEOUT)
+    driver.implicitly_wait(gv.MAX_TIMEOUT)
     yield driver
     driver.quit()
 
-@pytest.fixture()
+@pytest.fixture
 def site_auth(driver):
-    driver.get(MAIN_PAGE)
-    driver.find_element(*USERNAME_FIELD).send_keys(LOGIN)
-    driver.find_element(*PASSWORD_FIELD).send_keys(PASSWORD)
-    driver.find_element(*LOGIN_BUTTON).click()
+    driver.get(gv.MAIN_PAGE)
+    driver.find_element(*l.USERNAME_FIELD).send_keys(gv.LOGIN)
+    driver.find_element(*l.PASSWORD_FIELD).send_keys(gv.PASSWORD)
+    driver.find_element(*l.LOGIN_BUTTON).click()
+    return driver
+
+@pytest.fixture
+def reset_app_state(site_auth, driver):
+    driver.find_element(*l.BURGER_MENU).click()
+    driver.find_element(*l.RESET_APP_STATE).click()
+    driver.find_element(*l.CLOSE_BUTTON).click()
+
+@pytest.fixture
+def logged_user_emptied_cart(driver):
+    driver.get(gv.MAIN_PAGE)
+    driver.find_element(*l.USERNAME_FIELD).send_keys(gv.LOGIN)
+    driver.find_element(*l.PASSWORD_FIELD).send_keys(gv.PASSWORD)
+    driver.find_element(*l.LOGIN_BUTTON).click()
+    driver.find_element(*l.BURGER_MENU).click()
+    driver.find_element(*l.RESET_APP_STATE).click()
+    driver.find_element(*l.CLOSE_BUTTON).click()
--- lesson2/Tests/test_suite_independent.py
+++ lesson2/Tests/test_suite_independent.py
@@ -1,3 +1,4 @@
 from global_variables import INVENTORY_PAGE
-def test_successful_login(site_auth, driver):
+def test_successful_login(site_auth):
+
     assert driver.current_url == INVENTORY_PAGE
