1664b303714639f59d33cbb48570e660a86fb433
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 15:34:57 -0700

    feat: ANY_CHILD_ELEMENT

---
+++ lesson3/Tests/herokuapp/locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+# Authorization
+USERNAME_FIELD = (By.XPATH, "//input[@data-test='username']")
+PASSWORD_FIELD = (By.XPATH, "//input[@data-test='password']")
+LOGIN_BUTTON = (By.XPATH, "//input[@data-test='login-button']")
+LOGIN_ERROR = (By.XPATH, "//div[@class='error-message-container error']")
+
+
+
+# Add/Remove Elements
+ADD_ELEMENT_BUTTON = (By.XPATH, "//button[text()='Add Element']")
+DELETE_ELEMENT_BUTTON = (By.XPATH, "//div[@id='elements']/button")
+DELETE_BUTTONS_CONTAINER = (By.XPATH, "//div[@id='elements']")
+
+# Other elements
+ANY_CHILD_ELEMENT = (By.XPATH, "./*")
