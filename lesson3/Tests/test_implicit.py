be0b848158b94b68ff6cbb153f0024613d09c556
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 08:20:54 -0700

    refactor: variables

--- lesson3/Tests/test_implicit.py
+++ lesson3/Tests/test_implicit.py
@@ -1,29 +1,9 @@
+import variables as v
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
-# Variables
-MAX_TIMEOUT = 60
-PASSWORD = "password"
-LOGIN = "login"
-SITE_TITLE_TEXT = "Практика с ожиданиями в Selenium"
-SUCCESS_MESSAGE_TEXT = "Вы успешно зарегистрированы!"
-
-# url
-MAIN_PAGE = "https://victoretc.github.io/selenium_waits/"
-
-# Locators
-SITE_TITLE = (By.XPATH, "//h1")
-START_TESTING_BUTTON = (By.XPATH, "//button[text()='Начать тестирование']")
-LOGIN_FIELD = (By.XPATH, "//input[@id='login']")
-PASSWORD_FIELD = (By.XPATH, "//input[@id='password']")
-AGREE_CHECKBOX = (By.XPATH, "//input[@id='agree']")
-REGISTRATION_BUTTON = (By.XPATH, "//button[@id='register']")
-LOADER_INDICATOR = (By.XPATH, "//div[@id='loader']")
-SUCCESS_MESSAGE = (By.XPATH, "//p[@id='successMessage']")
 
 @pytest.fixture
 def chrome_options():
@@ -35,27 +15,27 @@ def chrome_options():
 @pytest.fixture
 def driver(chrome_options):
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(MAX_TIMEOUT)
+    driver.implicitly_wait(v.MAX_TIMEOUT)
     yield driver
     driver.quit()
 
 def test_site_registration(driver):
-    driver.get(MAIN_PAGE)
-    assert driver.find_element(*SITE_TITLE).text == SITE_TITLE_TEXT,\
+    driver.get(v.MAIN_PAGE)
+    assert driver.find_element(*v.SITE_TITLE).text == v.SITE_TITLE_TEXT,\
         "Site title is not 'Практика с ожиданиями в Selenium'"
 
-    driver.find_element(*START_TESTING_BUTTON).click()
-    driver.find_element(*LOGIN_FIELD).send_keys(LOGIN)
-    driver.find_element(*PASSWORD_FIELD).send_keys(PASSWORD)
-    driver.find_element(*AGREE_CHECKBOX).click()
-    driver.find_element(*REGISTRATION_BUTTON).click()
-    assert driver.find_element(*LOADER_INDICATOR).is_displayed(),\
+    driver.find_element(*v.START_TESTING_BUTTON).click()
+    driver.find_element(*v.LOGIN_FIELD).send_keys(v.LOGIN)
+    driver.find_element(*v.PASSWORD_FIELD).send_keys(v.PASSWORD)
+    driver.find_element(*v.AGREE_CHECKBOX).click()
+    driver.find_element(*v.REGISTRATION_BUTTON).click()
+    assert driver.find_element(*v.LOADER_INDICATOR).is_displayed(),\
         "Loading indicator is not displayed"
 
     # Waiting for the loading indicator to disappear and a message to appear
-    WebDriverWait(driver, MAX_TIMEOUT).until(EC.all_of(
-            EC.invisibility_of_element_located(LOADER_INDICATOR),
-            EC.presence_of_element_located(SUCCESS_MESSAGE)))
+    WebDriverWait(driver, v.MAX_TIMEOUT).until(EC.all_of(
+            EC.invisibility_of_element_located(v.LOADER_INDICATOR),
+            EC.presence_of_element_located(v.SUCCESS_MESSAGE)))
     # Checking for a message
-    assert driver.find_element(*SUCCESS_MESSAGE).text == SUCCESS_MESSAGE_TEXT,\
+    assert driver.find_element(*v.SUCCESS_MESSAGE).text == v.SUCCESS_MESSAGE_TEXT,\
         "Message 'Вы успешно зарегистрированы' is not displayed"
