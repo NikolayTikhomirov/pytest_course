5c2bafeb6dea67f4460e035862846477408f58cd
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 10:16:57 -0700

    done: test_sleep.py

--- lesson3/Tests/test_sleep.py
+++ lesson3/Tests/test_sleep.py
@@ -2,20 +2,18 @@ import variables as v
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from time import sleep
 
 @pytest.fixture
 def chrome_options():
     options = Options()
     options.add_argument('--incognito')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     return options
 
 @pytest.fixture
 def driver(chrome_options):
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(v.MAX_TIMEOUT)
     yield driver
     driver.quit()
 
@@ -23,7 +21,7 @@ def test_site_registration(driver):
     driver.get(v.MAIN_PAGE)
     assert driver.find_element(*v.SITE_TITLE).text == v.SITE_TITLE_TEXT,\
         "Site title is not 'Практика с ожиданиями в Selenium'"
-
+    sleep(v.TIMEOUT)
     driver.find_element(*v.START_TESTING_BUTTON).click()
     driver.find_element(*v.LOGIN_FIELD).send_keys(v.LOGIN)
     driver.find_element(*v.PASSWORD_FIELD).send_keys(v.PASSWORD)
@@ -31,11 +29,7 @@ def test_site_registration(driver):
     driver.find_element(*v.REGISTRATION_BUTTON).click()
     assert driver.find_element(*v.LOADER_INDICATOR).is_displayed(),\
         "Loading indicator is not displayed"
-
-    # Waiting for the loading indicator to disappear and a message to appear
-    WebDriverWait(driver, v.MAX_TIMEOUT).until(EC.all_of(
-            EC.invisibility_of_element_located(v.LOADER_INDICATOR),
-            EC.presence_of_element_located(v.SUCCESS_MESSAGE)))
+    sleep(v.TIMEOUT)
     # Checking for a message
     assert driver.find_element(*v.SUCCESS_MESSAGE).text == v.SUCCESS_MESSAGE_TEXT,\
         "Message 'Вы успешно зарегистрированы' is not displayed"
