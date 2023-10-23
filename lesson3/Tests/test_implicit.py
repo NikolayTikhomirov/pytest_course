a3773fcdab8022f2c5d118925783927e4dadc2af
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 00:57:43 -0700

    refactor: spaces

--- lesson3/Tests/test_implicit.py
+++ lesson3/Tests/test_implicit.py
@@ -28,9 +28,8 @@ SUCCESS_MESSAGE = (By.XPATH, "//p[@id='successMessage']")
 @pytest.fixture
 def chrome_options():
     options = Options()
-    # options.add_argument('--window-size=100,100')
     options.add_argument('--incognito')
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     return options
 
 @pytest.fixture
