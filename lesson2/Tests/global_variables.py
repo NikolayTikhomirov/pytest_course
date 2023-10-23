bc3808f35da5ad3f8f43b60f2d9a344f6af32a56
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-22 18:57:47 -0700

    feat: add variables

--- lesson2/Tests/global_variables.py
+++ lesson2/Tests/global_variables.py
@@ -9,12 +9,13 @@ MAIN_PAGE = "https://www.saucedemo.com/"
 INVENTORY_PAGE = "https://www.saucedemo.com/inventory.html"
 CART_PAGE = "https://www.saucedemo.com/cart.html"
 COMPANY_PAGE = "https://saucelabs.com/"
+SHOPPING_CART_PAGE = "https://www.saucedemo.com/cart.html"
 CHECKOUT_STEP_1 = "https://www.saucedemo.com/checkout-step-one.html"
 CHECKOUT_STEP_2 = "https://www.saucedemo.com/checkout-step-two.html"
 CHECKOUT_COMPLETE = "https://www.saucedemo.com/checkout-complete.html"
 
 # Timeout
-TIMEOUT = 0.1
+TIMEOUT = 1
 MAX_TIMEOUT = 10
 
 # Inventory
