3cb49e622c12fda899bbdbdc9939a72b1759f3a8
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-23 12:07:06 -0700

    deleted: test_sucessfull_login.py

--- lesson2/Tests/test_sucessfull_login.py
+++
@@ -1,9 +0,0 @@
-from locators import USERNAME_FIELD, PASSWORD_FIELD, LOGIN_BUTTON
-from global_variables import LOGIN, PASSWORD, MAIN_PAGE, INVENTORY_PAGE
-
-def test_login_form(driver):
-    driver.get(MAIN_PAGE)
-    driver.find_element(*USERNAME_FIELD).send_keys(LOGIN)
-    driver.find_element(*PASSWORD_FIELD).send_keys(PASSWORD)
-    driver.find_element(*LOGIN_BUTTON).click()
-    assert driver.current_url == INVENTORY_PAGE
