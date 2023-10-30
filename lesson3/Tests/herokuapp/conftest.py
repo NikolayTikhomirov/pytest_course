5fb7ab7796951d3a253632f94c50c1cd63485160
Nick Tikhomirov <la8185324310@gmail.com>
2023-10-30 16:18:48 -0700

    fead: add wait(driver)

--- lesson3/Tests/herokuapp/conftest.py
+++ lesson3/Tests/herokuapp/conftest.py
@@ -2,12 +2,14 @@ import global_variables as GV
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture
 def chrome_options():
     options = Options()
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
+    # options.add_argument('--incognito')
+    options.add_argument('--window-size=1920,1080')
+    # options.add_argument('--headless')
     return options
 
 @pytest.fixture
@@ -16,3 +18,8 @@ def driver(chrome_options):
     driver.implicitly_wait(GV.MAX_TIMEOUT)
     yield driver
     driver.quit()
+
+@pytest.fixture
+def wait(driver):
+    wait = WebDriverWait(driver, timeout=GV.MAX_TIMEOUT)
+    return wait
